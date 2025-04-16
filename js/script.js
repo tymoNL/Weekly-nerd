fc1551694512591b468078850e69f487d17a6646
Tymo <tymo.smids@gmail.com>
2025-04-16 13:41:05 +0200

    added js detail header and img

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const files = [
+    { filename: "/pages/weekly_nerd-1.html?id=1", name: "Kilian Valkhof", date: "2025-02-05", location: "WBH01B17", time: "16:00 uur", image: "/images/killian_valkhof.jpg" },
+    { filename: "/pages/weekly_nerd-2.html?id=2", name: "Peter Paul Koch", date: "2025-02-12", location: "WBH01B17", time: "16:00 uur", image: "/images/ppt.jpg" },
+    { filename: "/pages/weekly_nerd-3.html?id=3", name: "Cassie Evans", date: "2025-03-07", location: "BPH00B02", time: "13:00 uur", image: "/images/cassie_evans.png" },
+    { filename: "/pages/weekly_nerd-4.html?id=4", name: "Jeremy Keith", date: "2025-03-12", location: "WBH01B17", time: "16:00 uur", image: "/images/jeremy_keith.jpg" },
+    { filename: "/pages/weekly_nerd-5.html?id=5", name: "Rosa", date: "2025-04-04", location: "WBH01B17", time: "16:00 uur" },
+    { filename: "/pages/weekly_nerd-6.html?id=6", name: "Niels leenheer", date: "2025-04-09", location: "WBH01B17", time: "16:00 uur", image: "/images/niels-leenheer.jpg" },
+    { filename: "/pages/weekly_nerd-7.html?id=7", name: "Nienke de Keijzer", date: "2025-04-16", location: "KSH00A11", time: "16:00 uur", image: "/images/nienke_de_keijzer.jpg" }
+];
+
+// Get the container element
+const container = document.querySelector('.cardGrid');
+const details = document.querySelector('.details');
+
+// Loop through the files and create cards dynamically
+if (container) {
+    files.forEach((file, index) => {
+        const card = `
+            <article class="card">
+                <a href="${file.filename}">
+                    <img src="${file.image}" alt="${file.name}" />
+                </a>
+                <h4>${file.name}</h4>
+                <p>
+                    <i class="fa-solid fa-clock"></i>&#160;<i>${file.time ? file.time : "Tijd onbekend"}</i><br />
+                    <i class="fa-solid fa-location-dot"></i>&#160;<small>${file.location ? file.location: ""}</small><br />
+                    <small>${file.date}</small><br />
+                    <a href="${file.filename}">Meer info</a>
+                </p>
+            </article>
+        `;
+
+        container.insertAdjacentHTML('beforeend', card);
+    });
+}
+
+if (details) {
+    // Check the URL for a specific "id" query parameter to match the selected person
+    const urlParams = new URLSearchParams(window.location.search);
+    const selectedPersonId = urlParams.get('id'); // e.g., ?id=1 for the first person
+
+    const selectedPerson = files.find((file, index) => file.filename.includes(`weekly_nerd-${selectedPersonId}.html`));
+
+    if (selectedPerson) {
+        const detailsHeader = `
+            <div class="detailHeader">
+                <h2>Weekly nerd #${selectedPersonId}</h2>
+                <p>
+                    <b>${selectedPerson.name}</b><br />
+                    <i>${selectedPerson.date}</i>
+                </p>
+                <p>
+                    <small><i class="fa-solid fa-clock"></i>&#160;${selectedPerson.time}</small><br /> 
+                    <small><i class="fa-solid fa-location-dot"></i>&#160;${selectedPerson.location}</small>
+                </p>
+            </div>
+        `;
+
+        
+
+        const detailsImage = `
+            <div class="detailsImage">
+                <img src="${selectedPerson.image}" alt="${selectedPerson.name}" />
+            </div>
+        `;
+
+        details.insertAdjacentHTML('afterBegin', detailsImage);
+        details.insertAdjacentHTML('afterBegin', detailsHeader);
+    }
+}
